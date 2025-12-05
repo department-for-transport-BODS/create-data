d892951a28e76157a057608286dae21e629deadf
Harrison Duffield <46687573+HarrisonDuffield@users.noreply.github.com>
2025-12-05 16:12:20 +0000

    Revert "bugfix: DBODS-462 SUPPORT EMAIL var rename (#1008)"
    This reverts commit 84d1ca1c78cf6a6f00b4e35acef057027babd1bf.

--- repos/fdbt-site/.jest/setEnvVars.ts
+++ repos/fdbt-site/.jest/setEnvVars.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 
 process.env.SERVICE_EMAIL_ADDRESS = 'mock-service-address@email.co.uk';
-process.env.SUPPORT_EMAIL_ADDRESS = 'bodshelpdesk@kainos.com';
-process.env.SUPPORT_PHONE_NUMBER = '0800 000 000';
+process.env.SUPPORT_EMAIL = 'bodshelpdesk@kainos.com';
+process.env.SUPPORT_PHONE = '0800 000 000';
