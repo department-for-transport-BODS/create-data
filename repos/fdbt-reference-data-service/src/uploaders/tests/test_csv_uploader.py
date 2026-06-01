68567b4411ef5edff6e2895f51a32632c6d429ce
Caelainn Brinkley <caelainn.brinkley@kainos.com>
2026-06-01 15:42:59 +0100

    Updating tests to match the queries called

--- repos/fdbt-reference-data-service/src/uploaders/tests/test_csv_uploader.py
+++ repos/fdbt-reference-data-service/src/uploaders/tests/test_csv_uploader.py
@@ -4,20 +4,30 @@ from csv_uploader.db_queries import *
 class TestClass:
     def test_public_name_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        result = public_name_query(bucket)
+        noc_bucket_region = "eu-west-2"
+        result = public_name_query(bucket, noc_bucket_region)
         assert result[3].__contains__(bucket)
+        assert result[3].__contains__(noc_bucket_region)
 
     def test_noc_table_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        result = noc_table_query(bucket)
+        noc_bucket_region = "eu-west-2"
+        result = noc_table_query(bucket, noc_bucket_region)
         assert result[3].__contains__(bucket)
+        assert result[3].__contains__(noc_bucket_region)
 
     def test_noc_lines_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        result = noc_lines_query(bucket)
+        noc_bucket_region = "eu-west-2"
+        result = noc_lines_query(bucket, noc_bucket_region)
         assert result[3].__contains__(bucket)
+        assert result[3].__contains__(noc_bucket_region)
 
     def test_stops_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        result = stops_query(bucket)
+        naptan_s3_key = "NaPTAN.csv"
+        naptan_bucket_region = "eu-west-2"
+        result = stops_query(bucket, naptan_s3_key, naptan_bucket_region)
         assert result[3].__contains__(bucket)
+        assert result[3].__contains__(naptan_s3_key)
+        assert result[3].__contains__(naptan_bucket_region)
