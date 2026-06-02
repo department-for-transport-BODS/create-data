51794d15c25763061891b5d36e52a7179dc99b80
Caelainn Brinkley <caelainn.brinkley@kainos.com>
2026-06-02 10:50:31 +0100

    Updating to use the naptan_bucket_region as the noc is from the same region

--- repos/fdbt-reference-data-service/src/uploaders/tests/test_csv_uploader.py
+++ repos/fdbt-reference-data-service/src/uploaders/tests/test_csv_uploader.py
@@ -4,24 +4,24 @@ from csv_uploader.db_queries import *
 class TestClass:
     def test_public_name_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        noc_bucket_region = "eu-west-2"
-        result = public_name_query(bucket, noc_bucket_region)
+        naptan_bucket_region = "eu-west-2"
+        result = public_name_query(bucket, naptan_bucket_region)
         assert result[3].__contains__(bucket)
-        assert result[3].__contains__(noc_bucket_region)
+        assert result[3].__contains__(naptan_bucket_region)
 
     def test_noc_table_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        noc_bucket_region = "eu-west-2"
-        result = noc_table_query(bucket, noc_bucket_region)
+        naptan_bucket_region = "eu-west-2"
+        result = noc_table_query(bucket, naptan_bucket_region)
         assert result[3].__contains__(bucket)
-        assert result[3].__contains__(noc_bucket_region)
+        assert result[3].__contains__(naptan_bucket_region)
 
     def test_noc_lines_bucket_insertion(self):
         bucket = "unit-test-bucket"
-        noc_bucket_region = "eu-west-2"
-        result = noc_lines_query(bucket, noc_bucket_region)
+        naptan_bucket_region = "eu-west-2"
+        result = noc_lines_query(bucket, naptan_bucket_region)
         assert result[3].__contains__(bucket)
-        assert result[3].__contains__(noc_bucket_region)
+        assert result[3].__contains__(naptan_bucket_region)
 
     def test_stops_bucket_insertion(self):
         bucket = "unit-test-bucket"
